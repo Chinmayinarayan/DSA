c4195d9d23d67df6a4cfc008e7c56c6a3ea5d636
Chinmayi Kumbalavati <chinmayikumbalavati@gmail.com>
2026-09-02 21:59:31 +0530

    Added time complexity(constant) examples

---
+++ 03_Time_Complexity/constant_time.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter number : ";
+    cin>>n;
+    int sum=n*(n+1)/2;
+    cout<<"Sum = "<<sum<<endl;
+    return 0;
+}
