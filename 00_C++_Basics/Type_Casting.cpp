3503173f7746980d81521911caf03a647ec76b11
Chinmayi Kumbalavati <chinmayikumbalavati@gmail.com>
2026-09-02 11:23:05 +0530

    C++ type conversion and type casting

---
+++ 00_C++_Basics/Type_Casting.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double a=100.99;
+    int b=(int)a;   // explicit type casting
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b<<endl;
+    return 0;
+}
---
+++ 00_C++_Basics/Type_Conversion.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=10;
+    double b=a; //int automatically converted into double
+    cout<<"a value = "<<a<<endl;
+    cout<<"b value = "<<b<<endl;
+    return 0;
+}
