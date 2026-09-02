c05a2ba614267a836146f3eae15197694689741d
Chinmayi Kumbalavati <chinmayikumbalavati@gmail.com>
2026-09-02 11:39:09 +0530

    Pre increment example

---
+++ 00_C++_Basics/Pre_Increament.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    int b=++a;  //first value of a will be increased ,then a value will be assigned to b
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b<<endl;
+    return 0;
+}
